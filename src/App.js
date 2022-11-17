7bda72d36e8275ee5bcb552e19487c8d3c84171a
Andreia <andreiacpafonso@gmail.com>
2022-11-17 17:09:40 +0000

    labcountries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import countriesInfo from './countries.json';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import axios from 'axios';
+
+
+
+
 
 function App() {
+  const [countries, setCountries] = useState(countriesInfo);
+
+ 
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => setCountries(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+
+  countries.sort((a, b) => a.name.common > b.name.common);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CountriesList countries={countries} />} />
+        <Route path="/countries" element={<CountriesList countries={countries} />} />
+      </Routes>
+      <CountriesList countries={countries} />
     </div>
   );
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+
+//     <div className="container">
+//       <div className="row">
+//         <CountriesList countries={countries} />
+//           <Route path="/" element={<Navbar />} />
+//           <Routes>
+//           <Route path="/" element={<CountriesList />} />
+//           <Route path="/:id" element={<CountryDetails />} />
+//             </Routes>
+//     </div>
+//     </div>
+//     </div>
+//   );
+// }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+function Navbar(props) {
+    return (
+      <div className="navbar">
+        <h1 className="title">WikiCountries</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
